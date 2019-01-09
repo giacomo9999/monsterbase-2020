bc3ba53aeb8fefb39b14ea240fe0ab5f5f1a914b
James Gary <gmarchesi888@gmail.com>
2019-01-09 17:21:27 -0500

    Placeholder components in place and rendering successfully.

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import Create from "./components/create.component";
+import Index from "./components/index.component";
+import Edit from "./components/edit.component";
+
+const App = () => (
+  <Container>
+    <h1>MONSTERBASE</h1>
+    <Create />
+    <Edit />
+    <Index />
+  </Container>
+);
 
 export default App;
