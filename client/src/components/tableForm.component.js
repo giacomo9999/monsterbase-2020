389e2a8e9214b5d92524208084db5e1111aaede5
James Gary <gmarchesi888@gmail.com>
2019-01-14 12:25:24 -0500

    Working on making components stateful.

--- client/src/components/tableForm.component.js
+++ client/src/components/tableForm.component.js
@@ -1,35 +1,36 @@
 import React, { Component } from "react";
-import { Button, Card} from "semantic-ui-react";
+import { Button, Card } from "semantic-ui-react";
 
 export default class TableForm extends Component {
   render() {
     const submitText = this.props.region ? "Update" : "Create";
     return (
-      <div>
-        <Card centered>
-          <Card.Content>
-            <div className="ui blue basic inverted center aligned segment">
-              <h4>{this.props.region}</h4>
+      <Card centered>
+        <Card.Content>
+          <div className="ui blue basic inverted center aligned segment">
+            <h4>{this.props.region || "New Region"}</h4>
+          </div>
+          <div className="ui form">
+            <div className="field">
+              <label>Region Name</label>
+              <input
+                type="text"
+                defaultValue={this.props.region || "enter name here"}
+              />
             </div>
-            <div className="ui form">
-              <div className="field">
-                <label>Region Name</label>
-                <input type="text" defaultValue={this.props.region} />
-              </div>
-            </div>
-          </Card.Content>
-          <Card.Content extra>
-            <div className="ui compact two buttons">
-              <Button basic color="grey">
-                {submitText}
-              </Button>
-              <Button basic color="grey">
-                Cancel
-              </Button>
-            </div>
-          </Card.Content>
-        </Card>
-      </div>
+          </div>
+        </Card.Content>
+        <Card.Content extra>
+          <div className="ui compact two buttons">
+            <Button basic color="grey">
+              {submitText}
+            </Button>
+            <Button basic color="grey">
+              Cancel
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
     );
   }
 }
--- client/src/components/userDashboard.component.js
+++ client/src/components/userDashboard.component.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 import axios from "axios";
-import EditableTableList from './editableTableList.component'
-import ToggleableTableForm from './toggleableTableForm.component'
-
+import EditableTableList from "./editableTableList.component";
+import ToggleableTableForm from "./toggleableTableForm.component";
 
 export default class UserDashboard extends Component {
   constructor(props) {
     super(props);
-    this.state = { encTables: [] };
+    this.state = {
+      encTables: [
+        {
+          regionName: "Plain Of The Standing Stones",
+          regionTable: [
+            [4, "Scorpion(Large)"],
+            [9, "Pilgrim"],
+            [12, "Toad(Poisonous)"],
+            [15, "Nomad"],
+            [20, "Pyrolisk"]
+          ]
+        },
+        {
+          regionName: "Scarab Mound - Level 3",
+          regionTable: [
+            [3, "Bugbear"],
+            [7, "Ogre"],
+            [9, "Ochre Jelly"],
+            [11, "Tick(Giant)"],
+            [13, "Cyclopskin"],
+            [16, "Devil(Lemure)"],
+            [20, "Symbiotic Jelly"]
+          ]
+        }
+      ]
+    };
   }
 
   // componentDidMount() {
@@ -21,14 +45,12 @@ export default class UserDashboard extends Component {
   //     });
   // }
 
-    
-
   render() {
     return (
       <div className="ui three column centered grid">
-        <div className='column'>
-        <EditableTableList />
-        <ToggleableTableForm isOpen={false}/>
+        <div className="column">
+          <EditableTableList />
+          <ToggleableTableForm isOpen={true} />
         </div>
       </div>
     );
