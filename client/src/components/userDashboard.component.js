2125eae0ab60eb2c9afea1755417cfbd9f82e15e
James Gary <gmarchesi888@gmail.com>
2019-02-13 18:08:19 -0500

    Still won't post to DB.  I think I'm closer to isolating the problem, though.

--- client/src/components/userDashboard.component.js
+++ client/src/components/userDashboard.component.js
@@ -11,34 +11,34 @@ const uuidv1 = require("uuid/v1");
 export default class UserDashboard extends Component {
   state = {
     encTables: [
-      {
-        regionName: "Plain Of The Standing Stones",
-        id: uuidv1(),
-        regionType: "Desert",
-        regionDifficulty: 2,
-        regionMonstersAndFreq: [
-          [4, "Scorpion (Large)"],
-          [9, "Pilgrim"],
-          [12, "Toad (Poisonous)"],
-          [15, "Nomad"],
-          [20, "Pyrolisk"]
-        ]
-      },
-      {
-        regionName: "Scarab Mound - Level 3",
-        id: uuidv1(),
-        regionType: "Dungeon Level",
-        regionDifficulty: 3,
-        regionMonstersAndFreq: [
-          [3, "Bugbear"],
-          [7, "Ogre"],
-          [9, "Ochre Jelly"],
-          [11, "Tick (Giant)"],
-          [13, "Cyclopskin"],
-          [16, "Devil (Lemure)"],
-          [20, "Symbiotic Jelly"]
-        ]
-      }
+      // {
+      //   regionName: "Plain Of The Standing Stones",
+      //   id: uuidv1(),
+      //   regionType: "Desert",
+      //   regionDifficulty: 2,
+      //   regionMonstersAndFreq: [
+      //     [4, "Scorpion (Large)"],
+      //     [9, "Pilgrim"],
+      //     [12, "Toad (Poisonous)"],
+      //     [15, "Nomad"],
+      //     [20, "Pyrolisk"]
+      //   ]
+      // },
+      // {
+      //   regionName: "Scarab Mound - Level 3",
+      //   id: uuidv1(),
+      //   regionType: "Dungeon Level",
+      //   regionDifficulty: 3,
+      //   regionMonstersAndFreq: [
+      //     [3, "Bugbear"],
+      //     [7, "Ogre"],
+      //     [9, "Ochre Jelly"],
+      //     [11, "Tick (Giant)"],
+      //     [13, "Cyclopskin"],
+      //     [16, "Devil (Lemure)"],
+      //     [20, "Symbiotic Jelly"]
+      //   ]
+      // }
     ]
   };
 
@@ -54,8 +54,17 @@ export default class UserDashboard extends Component {
   // passed down as prop to ToggleableTableForm
   handleCreateFormSubmit = table => {
     console.log("UserDashboard creating new table for " + table.regionName);
-    const newTable = this.createNewTable(table);
+    // const newTable = this.createNewTable(table);
+    const newTable = this.state.encTables[0];
     console.log("UD: New table: ", newTable);
+
+    axios
+      .post("http://localhost:4000/encountertable/add", newTable)
+      .then(res => console.log(res.data))
+      .catch(err => {
+        console.log("unable to save to database");
+      });
+
     // added a callback to setState- so it logs the state *after* state updates.
     this.setState(
       { encTables: this.state.encTables.concat(newTable) },
@@ -72,7 +81,7 @@ export default class UserDashboard extends Component {
       id: uuidv1(),
       regionType: attrs.regionType || "Region Type",
       regionDifficulty: attrs.regionDifficulty || 0,
-      regionMonstersAndFreq: [[-999, "Placeholder"]]
+      regionMonstersAndFreq: ["-999", "Placeholder"]
     };
   };
 
